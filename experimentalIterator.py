29b91b81562cb7f99debc82b2bf1edd1317e3457
EL_MAGO <jdbs1997@gmail.com>
2022-12-12 06:27:24 -0500

    Update class on implemented models and f Altare

--- experimentalIterator.py
+++ experimentalIterator.py
@@ -7,15 +7,77 @@ class experimentalIterator: # class construction
     global labelEncoder
     labelEncoder=LabelEncoder()
     from sklearn.model_selection import train_test_split #BustamJos3 for models # import train_test split function
-    from sklearn.model_selection import GridSearchCV # hyperparameter getter
+    global train_test
+    train_test=train_test_split
     from sklearn.ensemble import RandomForestClassifier #import RandomForest
+    global randomForestC
+    randomForestC = RandomForestClassifier()
     import seaborn as sns #visualization
     import matplotlib.pyplot as plt
     
-
     def __init__(self, X, y):
-        self._X = X #store as attribute X data of dataset
-        self._y = y #same as above but y
+        self._X = self.Prepocess_Spaceship_Titanic( X )#store as attribute X data of dataset
+        self._y = labelEncoder.fit_transform(y.values) #to numeric
+        self._preproX=0
+        self._XTrain=0
+        self._yTrain=0
+        self._XTest=0
+        self._yTest=0
+    
+    def Prepocess_Spaceship_Titanic(self, d):
+        for i in range(len(d)):
+            if (d['HomePlanet'].isna()[i] == True and type(d['Cabin'][i])!= float):
+                if d['Cabin'][i][0] in 'ABCT':
+                    d.loc[i,'HomePlanet']='Europa'
+                elif d['Cabin'][i][0] == 'G':
+                    d.loc[i,'HomePlanet'] = 'Earth'
+            elif (d['RoomService'].isna()[i]== True and d['CryoSleep'][i]==True) or (d['RoomService'].isna()[i]== True and d['Age'][i]<=12):
+                d.loc[i,'RoomService']= float(0)
+            elif (d['FoodCourt'].isna()[i]== True and d['CryoSleep'][i]==True) or (d['FoodCourt'].isna()[i]== True and d['Age'][i]<=12):
+                d.loc[i,'FoodCourt']= float(0)   
+            elif (d['ShoppingMall'].isna()[i]== True and d['CryoSleep'][i]==True) or (d['ShoppingMall'].isna()[i]== True and d['Age'][0]<=12):
+                d.loc[i,'ShoppingMall']= float(0) 
+            elif (d['Spa'].isna()[i]== True and d['CryoSleep'][i]==True) or (d['Spa'].isna()[i]== True and d['Age'][i]<=12):
+                d.loc[i,'Spa']= float(0) 
+            elif (d['VRDeck'].isna()[i]== True and d['CryoSleep'][i]==True) or (d['VRDeck'].isna()[i]== True and d['Age'][i]<=12):
+                d.loc[i,'VRDeck']= float(0)
+            elif d['CryoSleep'].isna()[i]== True and d['RoomService'][i]==float(0) and d['FoodCourt'][i]==float(0) and d['ShoppingMall'][i]==float(0) and d['Spa'][i]==float(0) and d['VRDeck'][i]== float(0):
+                d.loc[i,'CryoSleep']=True
+            elif (d['CryoSleep'].isna()[i]== True and d['RoomService'][i] != 0  and d['FoodCourt'][i] != 0):
+                d.loc[i,'CryoSleep'] = False
+            elif (d['VIP'].isna()[i]== True and d['HomePlanet'][i]=='Earth'):
+                d.loc[i,'VIP']=False
+            elif (d['VIP'].isna()[i]== True and type(d['Cabin'][i])!= float):
+                if d['Cabin'][i][0]=='G' or d['Cabin'][i][0]=='T':
+                    d.loc[i,'VIP']=False
+            elif (d['VIP'].isna()[i]== True and d['Age'][i]<25 and d['HomePlanet'][i]=='Europe'):
+                d.loc[i,'VIP']=False
+            elif (d['VIP'].isna()[i]== True and d['Age'][i]<18 and d['HomePlanet'][i]=='Mars'):
+                d.loc[i,'VIP']=False
+            elif (d['VIP'].isna()[i]== True and d['HomePlanet'][i] == 'Mars' and d['CryoSleep'][i]==True):
+                d.loc[i,'VIP']=False
+            elif (d['VIP'].isna()[i]== True and d['HomePlanet'][i] == 'Europa' and d['CryoSleep'][i] == True and d['Age'][i] < 25):
+                d.loc[i,'VIP'] = False
+            elif (d['Destination'].isna()[i]== True and d['Age'][i]>18 and d['RoomService'][i]== float(0) and d['FoodCourt'][i]== float(0) and d['ShoppingMall'][i]== float(0) and d['Spa'][i]== float(0) and d['VRDeck'][i]== float(0) and d['CryoSleep'][i]==False):
+                d.loc[i,'Destination']= 'TRAPPIST-1e'
+            elif i<(len(d)-4):
+                for j in range(1,5): #Suponiendo una familia de max 5 integrantes
+                    if type(d['Name'][i])!= float:
+                        if type(d['Name'][i+j])!= float:
+                            if d['Destination'].isna()[i]== True and (d['Name'][i].split()[1] == d['Name'][i+j].split()[1]):
+                                d.loc[i,'Destination']=d['Destination'][i+j]
+                    if d['HomePlanet'].isna()[i]== True and (d['PassengerId'][i][:4]==d['PassengerId'][i+j][:4]):
+                        d.loc[i,'HomePlanet']=d['HomePlanet'][i+j]
+
+            elif i>=(len(d)-4):
+                for j in range(1,len(d)-i): 
+                    if type(d['Name'][i])!= float:
+                        if type(d['Name'][i+j])!= float:
+                            if d['Destination'].isna()[i]== True and (d['Name'][i].split()[1] == d['Name'][i+j].split()[1]):
+                                d.loc[i,'Destination']=d['Destination'][i+j]
+                    if d['HomePlanet'].isna()[i]== True and (d['PassengerId'][i][:4]==d['PassengerId'][i+j][:4]):
+                        d.loc[i,'HomePlanet']=d['HomePlanet'][i+j]
+        return d
     
     def dtypeConvertion(self, dataFrame): #convert dtypes to best fit #previous to BustamJos3 preprocess
         df=dataFrame.copy()
@@ -58,7 +120,37 @@ class experimentalIterator: # class construction
         imputedNumeric=KNNImputer().fit_transform(XNumeric)
         from sklearn.preprocessing import StandardScaler # standardization
         imputedNumeric=StandardScaler().fit_transform(imputedNumeric) # standardization
-        return concat( (imputedNumeric, convertedOHE), axis=1 )
+        self._preproX = concat( (imputedNumeric, convertedOHE), axis=1 )
         #TODO
-        #1. evaluate standardization only on numeric cols and then concantenate
-        #2. make implementation of selected models according to discussed
+        # 1. Import and tun Neural Network
+
+    def trainTestSplit(self): #to implement models
+        self._XTrain, self._yTrain, self._XTest, self._yTest=train_test(self._preproX, self.y, test_size=0.33, random_state=41)
+    
+    def neuralNetwork(self):
+        import tensorflow as tf
+        from sklearn.model_selection import GridSearchCV
+        from tensorflow.keras.models import Sequential
+        from tensorflow.keras.layers import Dense
+        from tensorflow.keras.layers import Dropout
+        from scikeras.wrappers import KerasClassifier
+        from tensorflow.keras.constraints import MaxNorm
+        def create_model(neurons): # Function to create model, required for KerasClassifier
+            model = Sequential() # create model
+            model.add(Dense(neurons, input_shape=(14,), kernel_initializer='uniform', activation='linear', kernel_constraint=MaxNorm(4)))
+            model.add(Dropout(0.2))
+            model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+            # Compile model
+            model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+            return model
+        seed = 41 # fix random seed for reproducibility
+        tf.random.set_seed(seed)
+        model = KerasClassifier(model=create_model, epochs=100, batch_size=10, verbose=0) # create model
+        return model
+    
+    def gridCVHyperparameter(self, model, params, metric):
+        from sklearn.model_selection import GridSearchCV # hyperparameter getter
+        gridSearchCV=GridSearchCV(model, params, scoring=metric)
+        self.trainTestSplit()
+        gridSearchCV.fit(self._XTrain, self._yTrain)
+        return model()
